d763e62d54695a511232089f6beae2c11a61bd8a
Sabrey <limsabriey94@gmail.com>
2025-12-23 09:47:52 +0700

    Update

--- OnlineLearnning/src/views/NotFound.vue
+++ OnlineLearnning/src/views/NotFound.vue
@@ -1,5 +1,12 @@
 <template>
-    <div>
-        Not found
+    <div class="fix-container">
+        <div class="justify-content-center flex align-item-center">
+            <img src="/Images/NotFound2.png"/>
+        </div>
+        <div class="text-center">
+            <h1>Page Not Found</h1>
+            <Button label="Back to Home" icon="pi pi-home"></Button>
+        </div>
+        
     </div>
 </template>
