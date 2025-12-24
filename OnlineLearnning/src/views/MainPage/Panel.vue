bed948d6524537bd997f4d115ceeb17244b08980
Sabrey1 <limsabriey94@gmail.com>
2025-12-24 08:00:12 +0700

    Update

--- OnlineLearnning/src/views/MainPage/Panel.vue
+++ OnlineLearnning/src/views/MainPage/Panel.vue
@@ -2,12 +2,12 @@
     <div class="surface-700" style="height: 100vh; padding-top: 100px;">
         <div class="fix-container  ">
             <div class="grid">
-                <div class="col">
-                    <div class="text-5xl line-height-1 border-round-sm text-white">
-                        <h1>Unlock Your <br> Potential <br>
-                        Learn Without<br> Limits</h1>
+                <div class="col   justify-content-center align-items-center">
+                    <div class="text-5xl line-height-1 border-round-sm text-white ">
+                        <h2 class="mb-0">Unlock Your <br> <span class="gradient-text">Potential</span> <br>
+                        Learn Without<br> Limits</h2>
                     </div>
-                    <p class="text-white text-xl mt-0">Join thousands of learners mastering new skills with expert-<br>led courses, interactive projects, and personalized learning paths.</p>
+                    <p class="text-gray-400 text-xl ">Join thousands of learners mastering new skills with expert-<br>led courses, interactive projects, and personalized learning paths.</p>
                     <div class="flex gap-3">
                         <Button label="Explore Courses" icon="pi pi-arrow-right" iconPos="right"></Button>
                         <Button label="Watch Demo" icon="pi pi-caret-right"></Button>
@@ -17,7 +17,7 @@
                         <div class="col">
                             <div class="text-white">
                                 <div class="flex align-item-center gap-2 ">
-                                    <i class="pi pi-users text-xl mt-2"></i>
+                                    <i class="pi pi-users text-xl mt-2 text-teal-500"></i>
                                     <h2 class="m-0">50K+</h2>
                                 </div>
                                 <p class="m-0">Active Learners</p>
@@ -26,7 +26,7 @@
                         <div class="col">
                             <div class="text-white">
                                 <div class="flex align-item-center gap-2 ">
-                                    <i class="pi pi-book text-xl mt-2"></i>
+                                    <i class="pi pi-book text-xl mt-2 text-teal-500"></i>
                                     <h2 class="m-0">1,200++</h2>
                                 </div>
                                 <p class="m-0">Courses</p>
@@ -35,7 +35,7 @@
                         <div class="col">
                             <div class="text-white">
                                 <div class="flex align-item-center gap-2 ">
-                                    <i class="pi pi-graduation-cap text-xl mt-2"></i>
+                                    <i class="pi pi-graduation-cap text-xl mt-2 text-teal-500"></i>
                                     <h2 class="m-0">98%+</h2>
                                 </div>
                                 <p class="m-0">Success Rate</p>
@@ -43,9 +43,9 @@
                         </div>
                     </div>            
                 </div>
-                <div class="col">
-                    <div class="p-3 border-round-sm font-bold ">
-                       <Card style="width: 40rem; overflow: hidden">
+                <div class="col flex justify-content-center align-items-center">
+                    <div class="p-3 border-round-sm font-bold">
+                       <Card style="width: 30rem; overflow: hidden">
                             <template #header>
                                 <img alt="user header"  src="https://primefaces.org/cdn/primevue/images/usercard.png" />
                             </template>
@@ -60,6 +60,18 @@
 </template>
 <script setup>
 import Button from 'primevue/button';
-import Image from 'primevue/image';
 import Card from 'primevue/card';
 </script>
+
+<style scoped>
+.gradient-text {
+  background: linear-gradient(
+    90deg,
+    rgb(44, 164, 211) 0%,
+    rgb(14, 96, 163) 90%
+  );
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+
+</style>
