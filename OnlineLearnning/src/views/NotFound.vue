ba03de897bdc069aa04c7c1868df65f177f5be25
Sabrey <limsabriey94@gmail.com>
2025-12-24 11:20:56 +0700

    Update

--- OnlineLearnning/src/views/NotFound.vue
+++ OnlineLearnning/src/views/NotFound.vue
@@ -1,12 +1,11 @@
 <template>
-    <div class="fix-container">
-        <div class="justify-content-center flex align-item-center">
-            <img src="/Images/NotFound2.png"/>
-        </div>
-        <div class="text-center">
-            <h1>Page Not Found</h1>
-            <Button label="Back to Home" icon="pi pi-home"></Button>
-        </div>
-        
+     <div class="flex justify-content-center align-items-center h-screen">
+    <div class="text-center">
+      <img src="/Images/NotFound2.png"   />
+      <h1 class="m-0">Page Not Found</h1>
+      <p class="mt-0">The page you are looking for does not exist.</p>
+      <router-link to="/" ><Button label="Back to Home" icon="pi pi-home"></Button></router-link>
+      
     </div>
+  </div>
 </template>
